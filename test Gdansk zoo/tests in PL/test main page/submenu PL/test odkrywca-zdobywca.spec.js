22f54044774e56e38f1d24be328f5c45084ab815
Yuliia Manuilova <umanuilova7@gmail.com>
2025-07-10 14:00:32 +0200

    added a new tests

---
+++ test Gdansk zoo/tests in PL/test main page/submenu PL/test odkrywca-zdobywca.spec.js
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+    // open home page of Gdansk zoo
+  await page.goto('https://zoo.gdansk.pl/en/homepage/');
+    // accept cookies
+  await page.getByRole('button', { name: 'Zgadzam się' }).click();
+    // change language to Polish
+  await page.getByRole('link', { name: 'pl' }).click();
+    // navigate to 'Wizyta w ZOO'
+  await page.locator('#menu-item-39').getByRole('link', { name: 'Wizyta w ZOO' }).click();
+   // click on 'Odkrywca - Zdobywca' link
+   await page.locator('#content').getByRole('link', { name: 'Odkrywca - Zdobywca' }).click();
+});
